ac7d6d6d6dfa33bc6d808c1cb3839a1d5adc1aef
mcneb10 <37050201+mcneb10@users.noreply.github.com>
2021-12-14 14:58:40 -0600

    Add exit capability

--- game.cbl
+++ game.cbl
@@ -1,10 +1,17 @@
        IDENTIFICATION DIVISION.
       * Cobol game
        PROGRAM-ID. cobolgame.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 WS-GAMEOVER PIC X VALUE 'N'.
+       01 WS-KEY PIC 9(8) USAGE IS COMP.
        PROCEDURE DIVISION.
        CALL "initscr".
-       CALL "printw" USING "Hello, world!".
-       CALL "refresh".
-       CALL "getch".
+       CALL "timeout" USING 0.
+       PERFORM GAMELOOP UNTIL WS-GAMEOVER IS EQUAL TO 'Y'.
        CALL "endwin".
        STOP RUN.
+       GAMELOOP.
+       	CALL "getch" RETURNING WS-KEY.
+        IF WS-KEY IS EQUAL TO 27 THEN MOVE 'Y' TO  WS-GAMEOVER.
+
